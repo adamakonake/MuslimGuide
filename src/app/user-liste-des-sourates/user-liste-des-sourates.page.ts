b2dacf1dcb8b78443c93ffc3a72ed6b2e78f1091
Amadou Touré <Amadouit223@gmail.com>
2023-09-07 11:01:18 +0000

    tâche une, terminée

--- src/app/user-liste-des-sourates/user-liste-des-sourates.page.ts
+++ src/app/user-liste-des-sourates/user-liste-des-sourates.page.ts
@@ -7,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserListeDesSouratesPage implements OnInit {
   sourates=['Al-Fatiha','Al-Baqara','Al-Imran','An-Nisa','Al-Maaida','Al-Anaam','Al-A','Al-Araaf','Yunus','Hud','Yusuf','Ar-Ra','Ibrahim','Al-Hijr','An-Nahl','Al-Isra','Al-Kahf','Maryam','Al-Hajj','Al-Mumtahan','Al-Fath','Al-Hujur','Al-Mujadilah','Al-Hadid','Al-Mumtahana','Al-Ikamah','Al-Falaq','An-Nas','Al-As','Al-Ahzab','Al-Mulk','Al-Qalam','Al-Haa','Al-Mum','Al-Musallat','Al-Insala','Al-Mursalat','An-Naba','Al-Numaya','Al-Hijja','Al-Ahzab'];
-  /*recherche: string = '';*/
+  nom='Mamadou';
+  prenom='Daffé'
+  photo="../../assets/icon/mamadou_daffe.jpg";
   recherche: string='';
   get sourateFiltres() {
     return this.sourates.filter(sourate => sourate.toLowerCase().includes(this.recherche.toLowerCase()));
