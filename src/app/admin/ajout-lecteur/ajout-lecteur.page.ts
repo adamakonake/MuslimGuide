5d8e49794f428e8b3124f3b0c25345964786aa93
unknown <tour.idrissa@odc.local>
2023-09-08 18:45:33 +0000

    ok

---
+++ src/app/admin/ajout-lecteur/ajout-lecteur.page.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-ajout-lecteur',
+  templateUrl: './ajout-lecteur.page.html',
+  styleUrls: ['./ajout-lecteur.page.scss'],
+})
+export class AjoutLecteurPage implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,10 +7,27 @@ const routes: Routes = [
     loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
   },
   {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
+    path: '', redirectTo: 'home', pathMatch: 'full'
+
   },
+  {
+    path: 'radio',
+    loadChildren: () => import('./users/radio/radio.module').then( m => m.RadioPageModule)
+  },
+  // {
+  //   path: 'liste-radio',
+  //   // loadChildren: () => import('./user/liste-radio').then( m => m.ListeRadioPageModule)
+  // },
+  // {
+  //   path: 'bousole',
+  //   loadChildren: () => import('./users/bousole/bousole.module').then( m => m.BousolePageModule)
+  // },
+  {
+    path: 'ajout-lecteur',
+    loadChildren: () => import('./admin/ajout-lecteur/ajout-lecteur.module').then( m => m.AjoutLecteurPageModule)
+  },
+
+
 ];
 
 @NgModule({
