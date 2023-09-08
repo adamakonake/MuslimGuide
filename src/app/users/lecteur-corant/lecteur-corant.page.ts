ccce284e1e5b876141ed4223df22b3129d8b267b
unknown <sy.diakaridia@odc.local>
2023-09-08 12:18:36 +0000

    sy jai fini pour la liste des radio

--- src/app/users/lecteur-corant/lecteur-corant.page.ts
+++ src/app/users/lecteur-corant/lecteur-corant.page.ts
@@ -1,21 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 
+
+// Interface pour définir la structure d'une personne
+interface Personne {
+  nom: string;
+  description: string;
+}
+
 @Component({
   selector: 'app-lecteur-corant',
   templateUrl: './lecteur-corant.page.html',
   styleUrls: ['./lecteur-corant.page.scss'],
 })
+
+
 export class LecteurCorantPage implements OnInit {
 
   constructor() { }
 
   ngOnInit() {
   }
-
-  // onSearch(event) {
-  //   const searchTerm = event.target.value;
-  //   // Vous pouvez maintenant utiliser searchTerm pour effectuer votre recherche
-  //   console.log('Terme de recherche : ', searchTerm);
-  // }
 
 }
