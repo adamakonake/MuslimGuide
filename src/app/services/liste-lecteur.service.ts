7a3af007558bf99d1d060ef59a8663da4e71547a
Amadou Touré <Amadouit223@gmail.com>
2023-09-11 12:05:55 +0000

    correction du service liste-lecteurs

--- src/app/services/liste-lecteur.service.ts
+++ src/app/services/liste-lecteur.service.ts
@@ -1,31 +1,8 @@
 import { Injectable } from '@angular/core';
-import {ListeLecteur} from "../modele/liste-lecteur";
-import {AngularFireDatabase, AngularFireList} from "@angular/fire/compat/database";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ListeLecteurService {
 
-  listeLecteurRef$: AngularFireList<ListeLecteur>;
-
-  constructor(private database: AngularFireDatabase) {
-    this.listeLecteurRef$ = this.database.list('liste-lecteur');
-  }
-
-  AllListeLecteurs() {
-    return this.listeLecteurRef$.valueChanges();
-  }
-
-  ajouterListeLecteur(listeLecteur: ListeLecteur) {
-    this.listeLecteurRef$.push(listeLecteur);
-  }
-
-  mAJListeLecteur(key: string, listeLecteur: ListeLecteur) {
-    this.listeLecteurRef$.update(key, listeLecteur);
-  }
-
-  supprimerListeLecteur(key: string) {
-    this.listeLecteurRef$.remove(key);
-  }
 }
