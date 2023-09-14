7efafff14eec21525dcebc21c737c24cb7368a57
adama konake <adamakoanke@gmail.com>
2023-09-14 09:29:12 +0000

    play coran in footer

--- src/app/services/lecteur.service.ts
+++ src/app/services/lecteur.service.ts
@@ -3,17 +3,25 @@ import { Firestore } from '@angular/fire/firestore';
 import { Lecteur } from '../admin/ajout-lecteur/mode';
 import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
 import { Subject } from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { saveAs } from 'file-saver'
+import { Filesystem, Directory, Encoding } from '@capacitor/filesystem';
 
 @Injectable({
   providedIn: 'any'
 })
 export class LecteurService {
 
-  playEnd = new Subject<number>;
+  playPaused = new Subject<number>;
+  isInit = new Subject<boolean>;
+  playNext = new Subject<number>;
+  currentTime = new Subject<number>;
+  timeMax = new Subject<number>;
   audio = new Audio();
   index! : number;
+  sourateListAudio : any
 
-  constructor(private readonly firestore: Firestore) { }
+  constructor(private readonly firestore: Firestore, private http : HttpClient) { }
 
   addLecteur(lecteur: Lecteur) {
     const data = collection(this.firestore, "Lecteur");
@@ -28,22 +36,47 @@ export class LecteurService {
     
   }
 
-  playQuran(index : number, source : string){
+  initCoranAudio(id : any){
+    this.http.get("https://api.quran.com/api/v4/chapter_recitations/"+id).subscribe((result : any)=>{
+        this.sourateListAudio = result.audio_files;
+    });
+    this.isInit.next(true);
+  }
+
+  playQuran(index : number){
     if(this.index == index){
-      if(this.audio.paused)
+      if(this.audio.paused){
         this.audio.play()
-      else
+        this.playPaused.next(-1);
+      }else{
         this.audio.pause()
+        this.playPaused.next(this.index);
+      }
     }else{
       this.index = index;
-      this.audio.src = source;
+      this.playNext.next(this.index)
+      this.audio.src = this.sourateListAudio[index].audio_url;
       this.audio.load();
       this.audio.play();
     }
 
     this.audio.onended = (e) =>{
-      this.playEnd.next(this.index);
-    }    
+      if(this.index < this.sourateListAudio.length){
+        this.audio.src = this.sourateListAudio[index+1].audio_url;
+        this.audio.load();
+        this.audio.play();
+        this.index++
+        this.playNext.next(this.index);
+      }else{
+        this.playPaused.next(this.index);
+      }
+    }
+    this.audio.onloadeddata = (e)=>{
+      this.timeMax.next(this.audio.duration);
+    }
+    this.audio.ontimeupdate = (e)=>{
+      this.currentTime.next(this.audio.currentTime);
+    }
 
     // if(index == this.isPlay ){
     //   this.audio.pause()
@@ -59,5 +92,36 @@ export class LecteurService {
     //     this.audio.play();
     //   }
     // }
+  }
+
+  goToNext(){
+    if(this.index < this.sourateListAudio.length){
+      this.audio.src = this.sourateListAudio[this.index+1].audio_url;
+      this.audio.load();
+      this.audio.play();
+      this.index++
+      this.playNext.next(this.index);
+    }else{
+      this.playPaused.next(this.index);
+    }
+  }
+
+  goToPrev(){
+    if(this.index > 0 ){
+      this.audio.src = this.sourateListAudio[this.index-1].audio_url;
+      this.audio.load();
+      this.audio.play();
+      this.index--
+      this.playNext.next(this.index);
+    }else{
+      this.playPaused.next(this.index);
+    }
+  }
+
+  download(index : number,nom : string){
+
+    //Filesystem.downloadFile(this.sourateListAudio[index].audio_url)
+
+    saveAs(this.sourateListAudio[index].audio_url,nom)
   }
 }
