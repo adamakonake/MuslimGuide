dcdea5d54dc282cdb55cc145458b33315483f12e
sarancoul <sc523644@gmail.com>
2023-09-14 14:17:35 +0000

    voir reste

--- src/app/admin/services/ajoutannonce.service.ts
+++ src/app/admin/services/ajoutannonce.service.ts
@@ -1,25 +1,20 @@
   import { Injectable } from '@angular/core';
-  import { Firestore, collectionData } from '@angular/fire/firestore';
-  // import { AngularFirestore } from '@angular/fire/firestore';
-  import { DocumentData, DocumentReference, addDoc, collection, doc, setDoc, updateDoc } from 'firebase/firestore';
+  import { Firestore, collectionData,query,where } from '@angular/fire/firestore';
+  import { DocumentReference, QuerySnapshot, addDoc, collection, doc, getDocs, setDoc, updateDoc } from 'firebase/firestore';
   import { deleteDoc } from 'firebase/firestore';
-  import { Observable, map } from 'rxjs';
   import { Annonce } from 'src/app/users/models/annonce';
   import { AlertController } from '@ionic/angular';
-  import { Handler } from 'leaflet';
-  import { async } from '@angular/core/testing';
-
-
-
+import { map } from '@mobiscroll/angular/dist/js/core/util/misc';
+  
   @Injectable({
     providedIn: 'any'
   })
   export class AjoutannonceService {
     annonces : any[]=[];
-    
-    //ajouter une annonce depuis le sevice
+
     constructor(private readonly firestore: Firestore, private alertController : AlertController) {}
-    
+
+///////////////////////////ajouter une annonce depuis le sevice///////////////////////////////////
     async addannonce(annonce: Annonce): Promise<DocumentReference> {
       // Vérifiez si tous les champs sont remplis
       if (!annonce.date || !annonce.nomMosquee || !annonce.heurePreche || !annonce.heureTabsir) {
@@ -35,14 +30,15 @@
         // Rejetez la promesse pour éviter d'ajouter le document incorrect
         return Promise.reject('Les champs obligatoires ne sont pas définis.');
       }
-    
       const document= collection(this.firestore, "Annonce");
       console.log(annonce);
       return addDoc(document,{
+        
         date : annonce.date,
         nomMosquee : annonce.nomMosquee,
         heurePreche : annonce.heurePreche,
         heureTabsir : annonce.heureTabsir,
+       
       });
     
     }
@@ -50,12 +46,10 @@
     getlistannonce(){
       const document = collection(this.firestore, "Annonce");
       // this.annonces = [];
-      
-    
       return collectionData(document, {idField : 'id'})//.subscribe((result : any[]) => { 
     }
 
-    //methode de modification de la liste des annonces
+////////////////////////methode de modification de la liste des annonces/////////////////////////////////
     async updateannonce(index : string, annonce:any){
       const modal = await this.alertController.create({
         header: 'Modification',
@@ -114,7 +108,7 @@
             } catch (error) {
               console.error('Erreur lors de la mise à jour : ', error);
 
-              // Affichez un message d'erreur à l'utilisateur
+            ///// Affichez un message d'erreur à l'utilisateur/////////
               const erreur = await this.alertController.create({
                 header: 'Erreur',
                 message: 'Une erreur s\'est produite lors de la mise à jour de l\'annonce.',
@@ -131,7 +125,7 @@
           await modal.present();
               }
 
-    // Méthode pour supprimer une annonce par ID
+ /////////////////////////// Méthode pour supprimer une annonce par ID////////////////////////////////////////
     async deleteAnnonceById(annonceId: string): Promise<void> {
       try {
         const documentRef = doc(this.firestore, 'Annonce', annonceId);
@@ -157,5 +151,60 @@
         console.error('Erreur lors de la suppression : ', error);
       }
     }
-  }    
 
+///////////////////METHODES POUR RECUPERER UNIQUEMENTS LES NOM DES MOSQUES///////////////////
+   
+    async getNomsMosquees(): Promise<string[]> {
+      const querySnapshot = await getDocs(query(collection(this.firestore, 'Annonce')));
+  
+      const nomsMosquees: string[] = [];
+  
+      for (const doc of querySnapshot.docs) {
+        const data = doc.data();
+        if (data['nomMosquee']) {
+           nomsMosquees.push(data['nomMosquee']);
+        }
+      }
+  
+      return nomsMosquees;
+    }
+    //////////////////METHODES POUR RECUPERER LES AUTRES DONNEES POUR DETAILS MOSQUEES///////////////////
+    async getDetailleMosquees(): Promise<any[]> {
+      
+      const querySnapshot = await getDocs(query(collection(this.firestore, 'Annonce')));    // Récupération de la collection 'Annonce' depuis Firestore
+    
+      const tableaudetailMosque: any[] = [];             // Tableau qui contiendra les détails des mosquées
+    
+      for (const doc of querySnapshot.docs) {             // Parcours de chaque document de la collection
+        
+        const data = doc.data();                   // Extraction des données du document
+        console.log(data);
+        
+        if (data['date'] ,data['heureTabsir']) {        // Vérification si les données 'date', 'heurePreche' et 'heureTabsir' existent
+          
+          const detailaimporter = data;             // Création d'une chaîne de caractères avec les détails
+               
+          tableaudetailMosque.push(detailaimporter); // Ajout de la chaîne de caractères au tableau des détails
+        }
+      }
+      return tableaudetailMosque;// Retourne le tableau des détails des mosquées
+    }
+     
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// `${data['date']}, ${data['heurePreche']}, ${data['heureTabsir']}`;
+// && data['heurePreche'] 
