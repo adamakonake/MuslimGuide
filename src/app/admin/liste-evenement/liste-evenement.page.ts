ff7aa24a71810eba806816f6e8022d29c31f8ce8
sarancoul <sc523644@gmail.com>
2023-09-19 18:10:03 +0000

    modification liste even

--- src/app/admin/liste-evenement/liste-evenement.page.ts
+++ src/app/admin/liste-evenement/liste-evenement.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Firestore } from '@angular/fire/firestore';
+// import { error } from 'console';
+import { AjoutenvenService } from 'src/app/services/ajoutenven.service';
 
 @Component({
   selector: 'app-liste-evenement',
@@ -6,10 +9,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-evenement.page.scss'],
 })
 export class ListeEvenementPage implements OnInit {
+  evenement: any;
+  evenementSaved: any;
+  
 
-  constructor() { }
+  constructor(private firestore : Firestore,
+              private ajoueven : AjoutenvenService) { }
 
   ngOnInit() {
+    this.getAllEvent();
+  }
+
+  //////////code pour la barre de recherche///////////////
+  onSearch(ev: any){
+  console.log(ev.target.value);
+    console.log('pas de rechche');
+  this.evenement= this.evenementSaved;
+  const filteredData = this.evenement.filter((even: any)=>even.lieu.includes(ev.target.value));
+  this.evenement = filteredData;
+  console.log(filteredData);
+  console.log('recherche effectuer')
+  }
+  /////////RECUPERATION DE LA METHODE DE RECUPERATION DANS LE SERVICE/////////////////////////
+  getAllEvent(){
+    this.ajoueven.getlistevenment().subscribe(result =>{
+      this.evenement = result;
+      this.evenementSaved = result;
+      console.log(this.evenement, 'je suis raatrapper');
+
+    });}
+
+  //////////////////////////modification///////////////////////////////////
+  updateeven(evenement : any){
+    this.ajoueven.updateeven(evenement.id, evenement);
+  }
+
+  //////////////RECUPERATION DE LA METHODE DE SUPPRESSION//////////////////
+  async deleteEvenById(evenementId : string): Promise<void>{
+    try{
+      await this.ajoueven.deleteEvenById(evenementId);
+      console.log('Suppression effectuer')
+    }catch(error){
+      console.error('Erreur')
+    }
   }
 
 }
--- src/app/services/ajoutenven.service.ts
+++ src/app/services/ajoutenven.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Firestore } from '@angular/fire/firestore';
-import { addDoc, collection, doc, setDoc } from 'firebase/firestore';
+import { Firestore, collectionData } from '@angular/fire/firestore';
+import { Router } from '@angular/router';
+import { addDoc, collection, deleteDoc, doc, setDoc, updateDoc } from 'firebase/firestore';
 import { Even } from '../users/models/even';
+import { AlertController } from '@ionic/angular';
 
 
 @Injectable({
@@ -9,8 +11,14 @@ import { Even } from '../users/models/even';
 })
 export class AjoutenvenService {
 
-  constructor( private readonly firestore: Firestore) { }
+  constructor( private readonly firestore: Firestore, private alertController : AlertController,
+    private router : Router) { }
 
+  navigueraj() {
+    this.router.navigateByUrl("/ajouter-evement");
+  }
+
+//////////////////ajout evenement /////////////////////////////////
   ajoutenven (evenement : Even){
     const document= collection(this.firestore, "Even");
     return addDoc(document,{
@@ -20,7 +28,119 @@ export class AjoutenvenService {
       heure : evenement.heure,
       description: evenement.description
     })
+
   }
 
+//////////////////////RECUPERATION EVENEMENT ///////////////////////////////////
+getlistevenment(){
+  const document = collection(this.firestore,"Even" )
+  return collectionData(document,{idField: 'id'});
+}
+
+/////////////MODIFICATION ///////////////////////////////
+async updateeven(index: string, evenement:any){
+  const modal = await this.alertController.create({
+    header : 'Modification',
+    inputs : [{
+      name : 'type',
+      type: 'text',
+      value : evenement.type
+    },
+    {
+      name: 'lieu',
+      type : 'text',
+      value : evenement.lieu
+    },
+    {
+      name: 'date',
+      type : 'date',
+      value: evenement.date
+    },
+    {
+      name : 'heure',
+      type: 'time',
+      value: evenement.heure
+    },
+    {
+      name : 'description',
+      type: 'text',
+      value: evenement.description
+    },
+  ],
+  buttons : [{
+    text : 'Annuler',
+    role : 'cancel'
+  },{
+    text : 'Modefier',
+    handler :async(data) => {
+      try{
+        const evenDoc = doc(this.firestore, 'Even', index);
+        await updateDoc(evenDoc,{
+          type: data.type,
+          lieu: data.lieu,
+          date: data.date,
+          heure: data.heure,
+          description : data.description });
+          //Afficher le message d'erreur
+          const confirmation = await this.alertController.create({
+            header : 'Confirmation',
+            message: 'Modification reussi',
+            buttons:['Ok'],
+          });
+          await confirmation.present();
+      }catch(error){
+        console.error('Erreur lors de la modification:' , error);
+        //Afficher un mesage
+        const erreur = await this.alertController.create({
+          header: 'Erreur',
+          message: 'Erreure produite',
+          buttons : ['Ok']
+        });
+        await erreur.present();
+      }
+      
+    },
+  }
+],
+  });
+  await modal.present();
+}
+
+///////////////////////////// SUPPRESSION////////////////////////////////////
+async deleteEvenById(evenId: string): Promise<void> {
+  try {
+    const documentRef = doc(this.firestore, 'Even', evenId);
+    
+    // Désactivez le bouton "Supprimer" jusqu'à ce que l'utilisateur fasse un choix dans la boîte de dialogue
+    const supp = await this.alertController.create({
+      header: 'Suppression',
+      message: 'Attention, voulez-vous vraiment supprimer ?',
+      buttons: [
+        {
+          text: 'Non',
+          role: 'cancel',
+        },
+        { 
+          text: 'Oui',
+          handler: async () => {
+            try {
+              // Supprimez le document
+              await deleteDoc(documentRef);
+              // Suppression réussie,  afficher un message de confirmation ici si nécessaire
+            } catch (error) {
+              console.error('Erreur lors de la suppression : ', error);
+            }
+          },
+        },
+      ],
+    });
+
+    // Affichez la boîte de dialogue
+    await supp.present();
+  }
+   catch (error) {
+    console.error('Erreur lors de la création de la boîte de dialogue : ', error);
+  }
+}
 
 }
