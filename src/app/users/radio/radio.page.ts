618af523505e58b0f71e4e04ec10eb9a479e749c
unknown <tour.idrissa@odc.local>
2023-09-19 17:10:44 +0000

    tw1o

--- src/app/users/radio/radio.page.ts
+++ src/app/users/radio/radio.page.ts
@@ -9,17 +9,46 @@ import { Radio } from 'src/app/admin/list-radio/mode';
 })
 export class RadioPage implements OnInit {
   // on va declare radion ici
-  radios:any;
+  radio:any;
+  searchTerm:string=''
+  // radio: any;
 
   constructor(private radioService: RadioService) { }//ici ce constructeur avec parametre
 
   ngOnInit() {
     //pour appel mon liste dans TypeScripte
     this.radioService.getRadio().subscribe((result)=>{
-      this.radios=result;
+      this.radio=result;
     })
     // ici ce pour afficher la liste de radio
-    console.log(this.radios);
+    console.log(this.radio);
   }
+  //pour faire la filter de rechrcher de radio liste
+  searchRadio(searchTerm: string){
+    if(!searchTerm){
+      return this.radio;
+    }
+    return this.radio.filter((radio:{name: string})=>
+    radio.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+  }
+  //ici recherch tourne sur texte recherch dans liste radio 
+  highlight(text: string, search:string ): string{
+    if(!search){
+      return text;
+    }
+    const pattern = search.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    const regex = new RegExp(pattern, "gi");
+    return text.replace(regex, match=> `<b>${match}</b>`);
+  }
+  sortRadio(searchTerm: string){
+    if(!searchTerm){
+      return this.radio;
+    }
+    return this.radio.sort((a:any, b:{nom: string;})=>
+    b.nom.toLowerCase().includes(searchTerm.toLowerCase()) ? 1:-1
+  );
+  }
+
 
 }
